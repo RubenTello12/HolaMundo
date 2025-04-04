aab1574e7eb7df544365f1319dbabe724d4eb489
RubenTello12 <93735005+RubenTello12@users.noreply.github.com>
2025-04-04 11:39:40 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
   public static void main(String[] args) {
-    System.out.println("Hola mundo desde un repositorio Github!!!!")
+    System.out.println("Hola mundo desde un repositorio Github!!!!");
   }
 }
